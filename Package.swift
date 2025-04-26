4b1724da7f3053585a48c2f931b27dd515767718
candronache <andronache.costin@gmail.com>
2025-04-26 15:32:04 +0300

    current

--- Package.swift
+++ Package.swift
@@ -5,10 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "swiftGLRendering",
+    dependencies: [
+        .package(path: "./swiftGLFW"),
+        .package(path: "../SwiftGLOpenGL")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "swiftGLRendering"),
+            name: "swiftGLRendering",
+            dependencies: [
+                .product(name: "swiftGLFW", package: "swiftGLFW"),
+                .product(name: "SGLOpenGL", package: "SwiftGLOpenGL")
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-L./swiftGLFW/Sources/swiftGLFW/glfw-3.4.bin.WIN64/lib-vc2022"])
+            ])
     ]
 )
--- Sources/Main.swift
+++ Sources/Main.swift
@@ -1,9 +1,39 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import swiftGLFW
+import SGLOpenGL
+
 @main
 struct Main {
     static func main() {
+        let width: Int32 = 800
+        let height: Int32 = 600
+        glfwInit()
+        defer { glfwTerminate() }
+    
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE)
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
 
+        guard let window = glfwCreateWindow(width, height, "LearnSwiftGL", nil, nil) else {
+            print("Failed to create GLFW window")
+            return
+        }
+
+        glfwMakeContextCurrent(window)
+        glViewport(x: 0, y: 0, width: width, height: height)
+
+        while glfwWindowShouldClose(window) == GL_FALSE
+        {
+            glfwPollEvents()
+
+            glClearColor(red: 0.2, green: 0.3, blue: 0.3, alpha: 1.0)
+            glClear(GL_COLOR_BUFFER_BIT)
+    
+            glfwSwapBuffers(window)
+        }
     }
 }
