b64e66932bbd73884164f18c56ef68d24468bb7e
candronache <andronache.costin@gmail.com>
2025-05-13 23:27:35 +0300

    current

--- Sources/App/DemoMeshLoader.swift
+++ Sources/App/DemoMeshLoader.swift
@@ -1,3 +1,8 @@
+struct Vec3 {
+    let x: Float
+    let y: Float
+    let z: Float
+}
 
 func loadBasicTriangle<GLW: GLWrapper>(_ t: GLW.Type) throws -> Mesh<GLW> {
     let vertices: [Float] = [
@@ -6,10 +11,16 @@ func loadBasicTriangle<GLW: GLWrapper>(_ t: GLW.Type) throws -> Mesh<GLW> {
         -0.5, -0.5, 0.0,  0.0, 1.0, 0.0,  // bottom left
          0.0,  0.5, 0.0,  0.0, 0.0, 1.0   // top 
     ];
+
+    let verticesv2: [(vertex: Vec3, color: Vec3)] = [
+        (Vec3(x: 0.5, y: 0.5, z: 0.0), .init(x: 1.0, y: 0.0, z: 0.0)),
+        (Vec3(x: -0.5, y: -0.5, z: 0.0), .init(x: 0.0, y: 1.0, z: 0.0)),
+        (Vec3(x: 0.5, y: -0.5, z: 0.0), .init(x: 0.0, y: 0.0, z: 1.0)),
+    ]
         let program = try GLW.createProgramWith(vertexShaderSource: basicVertexColorShader, 
                                                                         fragmentShaderSource: basicFragmentColorShader)
         let vertexBuffer = GLW.createVertexBuffer()
-        try vertices.withUnsafeBufferPointer { ptr in
+        try verticesv2.withUnsafeBufferPointer { ptr in
             let rawBufferPtr = UnsafeRawBufferPointer(ptr)
             try vertexBuffer.upload(data: rawBufferPtr, 
                                     describedAs: [
@@ -20,6 +31,6 @@ func loadBasicTriangle<GLW: GLWrapper>(_ t: GLW.Type) throws -> Mesh<GLW> {
         }
 
         return Mesh<GLW>(program: program, 
-                    vertexBuffer: vertexBuffer, 
-                    geometry: .init(drawMode: .triangles, vertexCount: 3))
+                         vertexBuffer: vertexBuffer, 
+                         geometry: .init(drawMode: .triangles, vertexCount: verticesv2.count))
 }
